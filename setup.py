477fe27fee09b1127f99b8d7f26e1621b3d50c74
arthur <a.s.tabakov@tinkoff.ru>
2025-04-01 14:16:12 +0700

    fix

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
-# Copyright 2023 DeepMind Technologies Limited.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Module setuptools script."""
-
 from setuptools import setup
 
 description = (
-    "GraphCast: Learning skillful medium-range global weather forecasting"
+    "Colab GraphCast: Learning skillful medium-range global weather forecasting"
 )
 
 setup(
-    name="graphcast",
+    name="colab_graphcast",  # новое имя пакета
     version="0.2.0.dev",
     description=description,
     long_description=description,
@@ -28,7 +13,8 @@ setup(
     license="Apache License, Version 2.0",
     keywords="GraphCast Weather Prediction",
     url="https://github.com/deepmind/graphcast",
-    packages=["graphcast"],
+    packages=["colab_graphcast"],  # имя пакета, под которым он будет импортироваться
+    package_dir={"colab_graphcast": "graphcast"},  # указываем, что исходный код в папке graphcast
     install_requires=[
         "cartopy",
         "chex",
